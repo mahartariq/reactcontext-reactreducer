26c5208e02d20c3b46b110acf6b2a9700a3e216a
mahartariq <mahartariq499@gmail.com>
2020-12-08 14:09:31 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {Balance} from './components/Balance';
 import {IncomeExpense} from './components/IncomeExpense';
 import {TransactionList} from './components/TransactionList';
 import {AddTransaction} from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 
 function App() {
   return (
-    <div className="App">
+   <GlobalProvider>
       <Header/>
       <div className="container">
         <Balance/>
@@ -17,7 +18,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+      </GlobalProvider>
   );
 }
 
